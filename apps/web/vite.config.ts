dd7e6276d9c6e608c75e4b4ae107871e5dbb7a5b
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-11-29 16:05:30 -0300

    fix: change some stuff in the velite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { API_URL } from "./src/lib/contants.ts";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/auth": {
-        target: API_URL,
+        target: "http://localhost:4321",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/auth/, "/auth"),
       },
